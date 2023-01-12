56d83a0f86d86307b4935450d53c7941336f2a04
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2023-01-12 16:31:55 +0100

    [ADD] Adding steps to test case 20

--- src/test/java/steps/CestaVon_StatisticsSteps.java
+++ src/test/java/steps/CestaVon_StatisticsSteps.java
@@ -11,8 +11,7 @@ import utility.Validation;
 import java.util.HashMap;
 import java.util.List;
 
-import static page.CestaVon_CommonPage.MainPage.*;
-import static page.CestaVon_UsersPage.usersPageItems.*;
+import static page.CestaVon_CommonPage.MainPage.SaveUserListFromStatistics;
 public class CestaVon_StatisticsSteps extends TestStepActions {
 
     static TestRunner TestRunner = new TestRunner();
@@ -107,41 +106,49 @@ public class CestaVon_StatisticsSteps extends TestStepActions {
 //        ReportExtender.logScreen(driver);
 //    }
 
-    @And("Save users list in loop")
-    public void saveUsersListInLoop() {
-        int allUsersDysplayedOnOnePage = 10;
-        indexparameters.put(1,"Všetko"); indexparameters.put(5,"aktívna"); indexparameters.put(9,"neaktívna");
-        indexparameters.put(2,"Omama"); indexparameters.put(6,"aktívna");
-        indexparameters.put(3,"Mentor"); indexparameters.put(7,"neaktívna");
-        indexparameters.put(4,"Supervízor"); indexparameters.put(8,"aktívna");
-        for (int i = 1; i < 4 ; i++) {
-            waitForElementClickable(driver, page.getDropdownLocator(indexparameters.get(i)), "Wait for dropdown " + indexparameters.get(i) + " is clickable", 10);
-            clickElementUsingJavascript(driver, page.getDropdownElement(indexparameters.get(i)), "Unwrap dropdown " + indexparameters.get(i));
-            waitForElementClickable(driver, page.getTabLocator(indexparameters.get(i+1)), "Wait for choice " + indexparameters.get(i+1) + " is visible", 10);
-            clickElementUsingJavascript(driver,page.getTabElement(indexparameters.get(i+1)), "Click on choice " + indexparameters.get(i+1));
-            boolean isEnabled = driver.findElement(NextPageButton.getLocator()).isEnabled();
-            for (int j = 0; j < 3 ; j++) {
-                int users_count = driver.findElements(page.getListActiveInactiveUserLocator(indexparameters.get(7+j),indexparameters.get(7-j))).size();
-
-
-
-
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-    }
+//    @And("Save users list in loop")
+//    public void saveUsersListInLoop() {
+//        String allUsersList = "";
+//        int allUsersDysplayedOnOnePage = 10;
+//        indexparameters.put(1,"Všetko"); indexparameters.put(5,"aktívna"); indexparameters.put(9,"neaktívna");
+//        indexparameters.put(2,"Omama"); indexparameters.put(6,"aktívna");
+//        indexparameters.put(3,"Mentor"); indexparameters.put(7,"neaktívna");
+//        indexparameters.put(4,"Supervízor"); indexparameters.put(8,"aktívna");
+//        for (int i = 1; i < 4 ; i++) {
+//            waitForElementClickable(driver, page.getDropdownLocator(indexparameters.get(i)), "Wait for dropdown " + indexparameters.get(i) + " is clickable", 10);
+//            clickElementUsingJavascript(driver, page.getDropdownElement(indexparameters.get(i)), "Unwrap dropdown " + indexparameters.get(i));
+//            waitForElementClickable(driver, page.getTabLocator(indexparameters.get(i+1)), "Wait for choice " + indexparameters.get(i+1) + " is visible", 10);
+//            clickElementUsingJavascript(driver,page.getTabElement(indexparameters.get(i+1)), "Click on choice " + indexparameters.get(i+1));
+//            boolean isEnabled = driver.findElement(NextPageButton.getLocator()).isEnabled();
+//            for (int j = 0; j < 3 ; j++) {
+//                int users_count = driver.findElements(page.getListActiveInactiveUserLocator(indexparameters.get(7+j),indexparameters.get(7-j))).size();
+//                ReportExtender.logInfo(Integer.toString(users_count));
+//                if (users_count > 0) {
+//                    while (users_count > 0) {
+//                        List<WebElement> users_list = driver.findElements(page.getListActiveInactiveUserLocator(indexparameters.get(7+j),indexparameters.get(7-j)));
+//                        for (WebElement user : users_list) {
+//                            allUsersList += user.getText() + " ";
+//                            indexparameters.put(10, allUsersList);
+//                        }
+//                        if (all_users_count == allUsersDysplayedOnOnePage && isEnabled) {
+//                            clickElementUsingJavascript(driver, NextPageButton.getElement(driver), NextPageButton.getDescription());
+//                            waitForFullPageLoad(driver, 10);
+//                            all_users_count = driver.findElements(page.getListActiveInactiveUserLocator("aktívna")).size();
+//                        } else break;
+//                    }
+//                } else {
+//                    textparameters.put("all_users_list", "Zoznam je prázdny");
+//                }
+//
+//
+//
+//
+//
+//
+//
+//            }
+//        }
+//    }
 
     @And("Open {string} statistics and verify details")
     public void openStatisticsAndVerifyDetails(String user) {
